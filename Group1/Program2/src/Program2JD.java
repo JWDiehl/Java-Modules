6ca79f2243eb600198a4f0d441dab36760f9492f
Jonathan Diehl <Jondiehl22@gmail.com>
2025-01-10 12:37:59 -0500

    Refine Program2JD

---
+++ Group1/Program2/src/Program2JD.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Program2JD {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter the number of tenants: ");
+        int tenantCount = input.nextInt();
+        
+        ArrayList<Tenant> tenants = createTenantList(tenantCount);
+        displayTenantDetails(tenants);
+    }
+
+    private static void displayTenantDetails(ArrayList<Tenant> tenants) {
+        
+    }
+
+    private static ArrayList<Tenant> createTenantList(int tenantCount) {
+    }
+}
