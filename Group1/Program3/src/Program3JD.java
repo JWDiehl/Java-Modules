c536c38d7257459bccb61cc5f603ff1ac4b2dedb
Jon Diehl <Jondiehl22@gmail.com>
2025-01-13 11:27:33 -0500

    ReadFileContents Method next

--- Group1/Program3/src/Program3JD.java
+++ Group1/Program3/src/Program3JD.java
@@ -4,7 +4,24 @@ public class Program3JD {
             System.err.println("error: please provide at least two arguments - text file name and Dale-Chall words file name.");
             return;
         }
+
+        //Text file name and DaleChallWordsname here
+        String textFileName = args[0];
+        String daleChallWordsFileName = args[1];
+
+        String text = readFileContents(textFileName);
+        if (text == null) {
+            System.err.println("Failed to read the text file. Exciting");
+            return;
+        }
+
+        Text newText = new Text(text);
+        DaleChall newDC = new DaleChall(newText, daleChallWordsFileName);
+        newDC.printGradeLevel();
     }
 
-    //Text file name and DaleChallWordsname here
+    private static String readFileContents(String textFileName) {
+    }
+
+
 }
