fb356601d3cc2d406c7a45e7ae2b8161062093e3
Jon Diehl <Jondiehl22@gmail.com>
2025-01-13 10:45:11 -0500

    Program 3 - TextFileName and DaleChallName

---
+++ Group1/Program3/src/Program3JD.java
@@ -0,0 +1,10 @@
+public class Program3JD {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.err.println("error: please provide at least two arguments - text file name and Dale-Chall words file name.");
+            return;
+        }
+    }
+
+    //Text file name and DaleChallWordsname here
+}
