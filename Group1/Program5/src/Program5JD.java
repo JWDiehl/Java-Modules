41432e8196270c4c3692b970e9797153a77db75d
Jon Diehl <Jondiehl22@gmail.com>
2025-01-22 19:05:29 -0500

    Program5JD

---
+++ Group1/Program5/src/Program5JD.java
@@ -0,0 +1,13 @@
+public class Program5JD {
+
+    //Employee Class
+    public class Employee {
+        private String name;
+        private String type;
+        private int hours;
+        private double wage;
+        private double salary;
+
+        //Constructor
+    }
+}
